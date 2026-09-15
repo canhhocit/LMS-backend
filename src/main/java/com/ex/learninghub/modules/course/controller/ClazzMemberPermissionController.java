a7a44c4d0e0774e4e9e56bf780e8334b4664586c
canhhocit <canhhuu26@gmail.com>
2026-09-15 22:15:03 +0700

    fix: Align ClazzMemberPermission route with context-path /api/v1

--- src/main/java/com/ex/learninghub/modules/course/controller/ClazzMemberPermissionController.java
+++ src/main/java/com/ex/learninghub/modules/course/controller/ClazzMemberPermissionController.java
@@ -24,7 +24,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/clazzes")
+@RequestMapping("/clazzes")
 @RequiredArgsConstructor
 public class ClazzMemberPermissionController {
 
